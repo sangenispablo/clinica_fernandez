c674f30cc6948317a2726ebb3ebac88d2297696a
Sangenis Pablo <sangenispablo@gmail.com>
2021-12-16 08:41:17 -0300

    doctors filtrado

--- js/doctors.js
+++ js/doctors.js
@@ -1,9 +1,19 @@
-import { getDoctors } from "./providers.js";
+import { getDoctorsFilter } from "./providers.js";
 
-getDoctors().then((data) => {
-     data.forEach((doctors) => {
-       const card = document.createElement("div");
-       card.innerHTML = `
+// lo primero que tengo que hacer es obtener todo lo que viene desde la url
+const valores = window.location.search;
+// saco solamente los parametros
+const urlParams = new URLSearchParams(valores);
+// pregunto si en esos parametros esta el "esp"
+let esp;
+if (urlParams.has("esp")) {
+  esp = urlParams.get('esp');  
+}
+
+getDoctorsFilter(esp).then((data) => {
+  data.forEach((doctors) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
             <div id="cardEsp" class="card-doctores m-3 border border-5 mb-3" style="width: 18rem; background: white">
                 <img src="${doctors.image}" class="card-img-top" alt="foto-doctor">
                 <h5 class="m-3">${doctors.esp}</h5>
@@ -12,7 +22,6 @@ getDoctors().then((data) => {
                 </div>
             </div>
             `;
-       document.getElementById("container-Doctor").appendChild(card);
-     });
-})
-
+    document.getElementById("container-Doctor").appendChild(card);
+  });
+});
